ef68ac1cd2b6f1b1d534e6afe3aba15ac3498f45
f33losopher <f33losopher@gmail.com>
2021-05-06 22:00:58 -0700

    Update tennis_score.py
    Update scoring tennis_score.py

--- tennis_score.py
+++ tennis_score.py
@@ -17,13 +17,38 @@ class TennisScore:
     # player1 won point
     # player2 lost point
     def update_game_score(self, player1, player2):
-        if self.match_score['game'][player1] == 0:
-            self.match_score['game'][player1] = 15;
-        elif self.match_score['game'][player1] == 15:
-            self.match_score['game'][player1] = 30;
-        elif self.match_score['game'][player1] == 30:
-            self.match_score['game'][player1] = 40;
-        elif self.match_score['game'][player1] == 40:
-            # TODO Finish Scoring
-            print "Need to check if it's deuce"
+        # TODO need to account for a tie break
+
+        game_score = self.match_score['game']
+        if game_score[player1] == 0:
+            game_score[player1] = 15
+        elif game_score[player1] == 15:
+            game_score[player1] = 30
+        elif game_score[player1] == 30:
+            game_score[player1] = 40
+        elif game_score[player1] == 40:
+            if game_score[player2] == 40:
+                game_score[player1] = 'Ad'
+                game_score[player2] = '-'
+            else:
+                self.update_set_score(player1, player2)
+                game_score[player1] = 0
+                game_score[player2] = 0
+        elif game_score[player1] == 'Ad':
+            self.update_set_score(player1, player2)
+            game_score[player1] = 0
+            game_score[player2] = 0
+        elif game_score[player1] == '-':
+            game_score[player1] = 40
+            game_score[player2] = 40
     
+    # player1 won game
+    # player2 lost game
+    def update_set_score(self, player1, player2):
+        set_score = self.match_score['set']
+        set_score[player1][-1] += 1
+
+        if ((set_score[player1][-1] == 6) and (set_score[player2][-1] < 5)) or set_score[player1][-1] == 7:
+            # Player1 wins set
+            set_score[player1].append(0)
+            set_score[player2].append(0)
