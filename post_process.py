a49a0d8d3f5cb59933285467f74dff9beb249d31
f33losopher <f33losopher@gmail.com>
2021-05-07 13:39:49 -0700

    Update post_process.py
    Bring new clips into new timeline, but the sorting is alphabetical

--- post_process.py
+++ post_process.py
@@ -3,6 +3,26 @@ from python_get_resolve import GetResolve
 from process_timeline_items import process_timeline
 from project_consts import *
 
+
+# Takes clips in media pool and creates 'Timeline'
+def create_timeline(resolve):
+    pm = resolve.GetProjectManager()
+    project = pm.GetCurrentProject()
+    mediaPool = project.GetMediaPool()
+
+    mediaPool.CreateEmptyTimeline('Timeline1')
+    project.SetCurrentTimeline('Timeline1')
+    mediaClips = mediaPool.GetRootFolder().GetClipList()
+
+    # mediaClips aren't in any order, sort it by 
+    # clip.GetName()
+    # Remove Timeline
+    mediaClips = filter(lambda k: k.GetName().startswith('clip'), mediaClips)
+    mediaClips.sort(key=lambda k: k.GetName())
+    mediaClips = filter(lambda k: k.GetName() != 'Timeline', mediaClips)
+    for clip in mediaClips:
+        mediaPool.AppendToTimeline(clip)
+
 resolve = app.GetResolve()
 resolve.OpenPage("edit")
 
@@ -13,3 +33,9 @@ project = projectManager.GetCurrentProject()
 # Trim unwanted parts of videos
 timeline = project.GetCurrentTimeline()
 process_timeline(timeline)
+
+# Time to import all the new clips
+mediaStorage = resolve.GetMediaStorage()
+mediaStorage.AddItemListToMediaPool(ROOT_MEDIA_FOLDER)
+
+create_timeline(resolve)
