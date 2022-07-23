5e9a4d1ef3215543991fcd5402e6ee2e43dedefd
f33losopher <f33losopher@gmail.com>
2022-07-23 09:30:06 -0700

    Update project_consts.py
    Have base path

--- project_consts.py
+++ project_consts.py
@@ -1,6 +1,7 @@
-BASE_SCOREBOARD = "C:\\Users\\fluuc\\AppData\\Roaming\\Blackmagic Design\\DaVinci Resolve\\Support\\Fusion\\Scripts\\Comp\\ScoreBoard.jpg"
-ERBOS_DRACO     = "C:\\Users\\fluuc\\AppData\\Roaming\\Blackmagic Design\\DaVinci Resolve\\Support\\Fusion\\Scripts\\Comp\\fonts\\erbos-draco-monospaced-nbp-font\\ErbosDraco1StOpenNbpRegular-l5wX.ttf"
-SELF_DESTRUCT   = "C:\\Users\\fluuc\\AppData\\Roaming\\Blackmagic Design\\DaVinci Resolve\\Support\\Fusion\\Scripts\\Comp\\fonts\\self-destruct-button-bb-font\\SelfdestructbuttonbbBold-0gKR.otf" 
+BASE_DAVINCI_PATH = "C:\\Users\\fluuc\\AppData\\Roaming\\Blackmagic Design\\DaVinci Resolve\\Support\\Fusion\\Scripts\\Comp\\"
+BASE_SCOREBOARD = BASE_DAVINCI_PATH + "ScoreBoard.jpg"
+ERBOS_DRACO     = BASE_DAVINCI_PATH + "fonts\\erbos-draco-monospaced-nbp-font\\ErbosDraco1StOpenNbpRegular-l5wX.ttf"
+SELF_DESTRUCT   = BASE_DAVINCI_PATH + "fonts\\self-destruct-button-bb-font\\SelfdestructbuttonbbBold-0gKR.otf" 
 
 PLAYER1 = 'Player1'
 PLAYER2 = 'Player2'
